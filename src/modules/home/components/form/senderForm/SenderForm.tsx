b2637fc525e6435b2740f69c43ec53e5bbb5d65f
Eze Medina <tec.medinaeze@gmail.com>
2026-07-29 21:25:41 -0300

    feat: add humboldt location

--- src/modules/home/components/form/senderForm/SenderForm.tsx
+++ src/modules/home/components/form/senderForm/SenderForm.tsx
@@ -16,7 +16,7 @@ interface Props {
 }
 
 const provinces = ['Santa Fe', 'Cordoba', 'Entre Rios', 'Buenos aires'];
-const citys = ['Santo Tome', 'Santa Fe', 'Rosario', 'Cordoba', 'Rafaela', 'Sunchales', 'San Francisco', 'Tacural', 'Esperanza', 'Coronda', 'CABA', 'GBA'];
+const citys = ['Santo Tome', 'Santa Fe', 'Rosario', 'Cordoba', 'Rafaela', 'Sunchales', 'Humboldt', 'San Francisco', 'Tacural', 'Esperanza', 'Coronda', 'CABA', 'GBA'];
 const types = ['DNI', 'CUIL', 'CUIT'];
 
 export const SenderForm = (props: Props) => {
